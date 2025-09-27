240cc315a96b7d3f46187410ecfa84196207ac11
Eshva <reg@eshva.ru>
2025-09-28 01:20:41 +0530

    Add required deployemnts
    Deployment is a solution component that should be deployed into test or benchmark environment to run them.

---
+++ code/solutions/Eshva.Caching.Nats.Tests.OutOfProcessDeployments/ObjectStoreBucket.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace Eshva.Caching.Nats.Tests.OutOfProcessDeployments;
+
+[PublicAPI]
+public readonly record struct ObjectStoreBucket(
+  string Name,
+  long? BucketVolumeBytes) {
+  public static ObjectStoreBucket Named(string bucketName) => new() { Name = bucketName };
+
+  public ObjectStoreBucket OfSize(long bucketVolumeBytes) => this with { BucketVolumeBytes = bucketVolumeBytes };
+}
