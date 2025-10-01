295d34b922a2cb689414d33398a6bf803e5026e0
Eshva <reg@eshva.ru>
2025-10-01 23:12:22 +0530

    some chnges from India

--- code/benchmarks/Eshva.Caching.Nats.TestWebApp/ObjectStoreBasedCache/GetImageHttpRequestHandler.cs
+++ code/benchmarks/Eshva.Caching.Nats.TestWebApp/ObjectStoreBasedCache/GetImageHttpRequestHandler.cs
@@ -1,5 +1,13 @@
-﻿namespace Eshva.Caching.Nats.TestWebApp.ObjectStoreBasedCache;
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace Eshva.Caching.Nats.TestWebApp.ObjectStoreBasedCache;
 
 public class GetImageHttpRequestHandler {
+  public GetImageHttpRequestHandler(IBufferDistributedCache cache) {
+    _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+  }
+
   public Task<IResult> Handle(string imageName) => Task.FromResult(Results.File([]));
+
+  private readonly IBufferDistributedCache _cache;
 }
