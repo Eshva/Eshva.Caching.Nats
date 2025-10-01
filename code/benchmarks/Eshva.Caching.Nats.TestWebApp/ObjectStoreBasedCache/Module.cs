6000f4d74ac1c694fa11ffdf04a8e6fabfe92bb8
Eshva <reg@eshva.ru>
2025-10-01 23:12:22 +0530

    some chnges from India

--- code/benchmarks/Eshva.Caching.Nats.TestWebApp/ObjectStoreBasedCache/Module.cs
+++ code/benchmarks/Eshva.Caching.Nats.TestWebApp/ObjectStoreBasedCache/Module.cs
@@ -1,8 +1,20 @@
-﻿namespace Eshva.Caching.Nats.TestWebApp.ObjectStoreBasedCache;
+﻿using Eshva.Caching.Abstractions;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Eshva.Caching.Nats.TestWebApp.ObjectStoreBasedCache;
 
 internal static class Module {
-  public static void AddServices(IServiceCollection services) =>
+  public static void AddServices(IServiceCollection services) {
+    services.AddTransient<IBufferDistributedCache, NatsObjectStoreBasedCache>();
+    services.AddTransient<ICacheEntryExpirationStrategy, StandardCacheEntryExpirationStrategy>(diContainer =>
+      new StandardCacheEntryExpirationStrategy(
+        new ExpirationStrategySettings { DefaultSlidingExpirationInterval = TimeSpan.FromMinutes(minutes: 5) },
+        diContainer.GetRequiredService<TimeProvider>()));
+    services.AddTransient<ICacheExpiredEntriesPurger, ObjectStoreBasedCacheExpiredEntriesPurger>(diContainer =>
+      new ObjectStoreBasedCacheExpiredEntriesPurger());
+
     services.AddTransient<GetImageHttpRequestHandler>();
+  }
 
   public static void MapEndpoints(IEndpointRouteBuilder endpoints) =>
     endpoints.MapGet(
