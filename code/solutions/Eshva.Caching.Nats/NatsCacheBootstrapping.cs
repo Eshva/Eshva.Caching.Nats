ccf75d2831c0223b72f43940a419b1f402a8a9ad
Mike Eshva <reg@eshva.ru>
2025-11-09 06:30:27 +0300

    Improve registration in DI-container
    - Add variants for single cache registration with keyed NATS-connection.
    - Add variants for single cache with single NATS-connection.
    - Register cache instance not only as IBufferDistributedCache but as IDistributedCache as well.

--- code/solutions/Eshva.Caching.Nats/NatsCacheBootstrapping.cs
+++ code/solutions/Eshva.Caching.Nats/NatsCacheBootstrapping.cs
@@ -32,28 +32,49 @@ public static class NatsCacheBootstrapping {
     string serviceKey,
     string natsServerKey) {
     services.AddKeyedSingleton<IBufferDistributedCache, NatsObjectStoreBasedCache>(
-      serviceKey,
-      (diContainer, key) => {
-        var natsClient = diContainer.GetRequiredKeyedService<INatsConnection>(natsServerKey);
-        var settings = diContainer.GetRequiredKeyedService<ObjectStoreBasedCacheSettings>(key);
-        var objectStoreContext = natsClient.CreateJetStreamContext()
-          .CreateObjectStoreContext();
-        var bucket = objectStoreContext.GetObjectStoreAsync(settings.BucketName).AsTask().GetAwaiter().GetResult();
-        var timeProvider = diContainer.GetRequiredService<TimeProvider>();
-        var expiryCalculator = new CacheEntryExpiryCalculator(settings.DefaultSlidingExpirationInterval, timeProvider);
-        var datastore = new ObjectStoreBasedDatastore(bucket, expiryCalculator);
-        var invalidation = new ObjectStoreBasedCacheInvalidation(
-          bucket,
-          settings.ExpiredEntriesPurgingInterval,
-          expiryCalculator,
-          timeProvider,
-          diContainer.GetRequiredService<ILogger<ObjectStoreBasedCacheInvalidation>>());
-        return new NatsObjectStoreBasedCache(
-          datastore,
-          invalidation,
-          diContainer.GetRequiredService<ILogger<NatsObjectStoreBasedCache>>());
-      });
+        serviceKey,
+        (diContainer, key) => CreateNatsObjectStoreBasedCache(
+          diContainer.GetRequiredKeyedService<INatsConnection>(natsServerKey),
+          diContainer.GetRequiredKeyedService<ObjectStoreBasedCacheSettings>(key),
+          diContainer))
+      .AddKeyedSingleton<IDistributedCache>(
+        serviceKey,
+        (diContainer, key) => diContainer.GetRequiredKeyedService<IBufferDistributedCache>(key));
+    return services;
+  }
 
+  /// <summary>
+  /// Add NATS object store based cache as a common service into DI-container.
+  /// </summary>
+  /// <remarks>
+  /// Cache can be placed on a separate NATS cluster. To separate server connection registrations the
+  /// <paramref name="natsServerKey"/> is used.
+  /// </remarks>
+  /// <param name="services">DI-container.</param>
+  /// <param name="natsServerKey">NATS server connection service key.</param>
+  /// <returns>Service collection.</returns>
+  public static IServiceCollection AddNatsObjectStoreBasedCache(this IServiceCollection services, string natsServerKey) {
+    services.AddSingleton<IBufferDistributedCache, NatsObjectStoreBasedCache>(diContainer =>
+        CreateNatsObjectStoreBasedCache(
+          diContainer.GetRequiredKeyedService<INatsConnection>(natsServerKey),
+          diContainer.GetRequiredService<ObjectStoreBasedCacheSettings>(),
+          diContainer))
+      .AddSingleton<IDistributedCache>(diContainer => diContainer.GetRequiredService<IBufferDistributedCache>());
+    return services;
+  }
+
+  /// <summary>
+  /// Add NATS object store based cache as a common service into DI-container.
+  /// </summary>
+  /// <param name="services">DI-container.</param>
+  /// <returns>Service collection.</returns>
+  public static IServiceCollection AddNatsObjectStoreBasedCache(this IServiceCollection services) {
+    services.AddSingleton<IBufferDistributedCache, NatsObjectStoreBasedCache>(diContainer =>
+        CreateNatsObjectStoreBasedCache(
+          diContainer.GetRequiredService<INatsConnection>(),
+          diContainer.GetRequiredService<ObjectStoreBasedCacheSettings>(),
+          diContainer))
+      .AddSingleton<IDistributedCache>(diContainer => diContainer.GetRequiredService<IBufferDistributedCache>());
     return services;
   }
 
@@ -74,35 +95,101 @@ public static class NatsCacheBootstrapping {
     string serviceKey,
     string natsServerKey) {
     services.AddKeyedSingleton<IBufferDistributedCache, NatsKeyValueStoreBasedCache>(
-      serviceKey,
-      (diContainer, key) => {
-        var natsClient = diContainer.GetRequiredKeyedService<INatsConnection>(natsServerKey);
-        var settings = diContainer.GetRequiredKeyedService<KeyValueBasedCacheSettings>(key);
-        var keyValueStoreContext = natsClient.CreateJetStreamContext().CreateKeyValueStoreContext();
-        var valueStore = keyValueStoreContext.CreateStoreAsync(settings.ValueStore).AsTask().GetAwaiter().GetResult();
-        var metadataStore = keyValueStoreContext.CreateStoreAsync(settings.MetadataStore).AsTask().GetAwaiter().GetResult();
-        var timeProvider = diContainer.GetRequiredService<TimeProvider>();
-        var expiryCalculator = new CacheEntryExpiryCalculator(settings.DefaultSlidingExpirationInterval, timeProvider);
-        var expirySerializer = new CacheEntryExpiryJsonSerializer();
-        var datastore = new KeyValueBasedDatastore(
-          valueStore,
-          metadataStore,
-          expirySerializer,
-          expiryCalculator);
-        var invalidation = new KeyValueBasedCacheInvalidation(
-          valueStore,
-          metadataStore,
-          settings.ExpiredEntriesPurgingInterval,
-          expirySerializer,
-          expiryCalculator,
-          timeProvider,
-          diContainer.GetRequiredService<ILogger<KeyValueBasedCacheInvalidation>>());
-        return new NatsKeyValueStoreBasedCache(
-          datastore,
-          invalidation,
-          diContainer.GetRequiredService<ILogger<NatsKeyValueStoreBasedCache>>());
-      });
-
+        serviceKey,
+        (diContainer, key) =>
+          CreateNatsKeyValueStoreBasedCache(
+            diContainer.GetRequiredKeyedService<INatsConnection>(natsServerKey),
+            diContainer.GetRequiredKeyedService<KeyValueBasedCacheSettings>(key),
+            diContainer))
+      .AddKeyedSingleton<IDistributedCache>(
+        serviceKey,
+        (diContainer, key) => diContainer.GetRequiredKeyedService<IBufferDistributedCache>(key));
     return services;
+  }
+
+  /// <summary>
+  /// Add NATS key-value store based cache as a common service into DI-container.
+  /// </summary>
+  /// <remarks>
+  /// Cache can be placed on a separate NATS cluster. To separate server connection registrations the
+  /// <paramref name="natsServerKey"/> is used.
+  /// </remarks>
+  /// <param name="services">DI-container.</param>
+  /// <param name="natsServerKey">NATS server connection service key.</param>
+  /// <returns>Service collection.</returns>
+  public static IServiceCollection AddNatsKeyValueBasedCache(this IServiceCollection services, string natsServerKey) {
+    services.AddSingleton<IBufferDistributedCache, NatsKeyValueStoreBasedCache>(diContainer =>
+        CreateNatsKeyValueStoreBasedCache(
+          diContainer.GetRequiredKeyedService<INatsConnection>(natsServerKey),
+          diContainer.GetRequiredService<KeyValueBasedCacheSettings>(),
+          diContainer))
+      .AddSingleton<IDistributedCache>(diContainer => diContainer.GetRequiredService<IBufferDistributedCache>());
+    return services;
+  }
+
+  /// <summary>
+  /// Add NATS key-value store based cache as a common service into DI-container.
+  /// </summary>
+  /// <param name="services">DI-container.</param>
+  /// <returns>Service collection.</returns>
+  public static IServiceCollection AddNatsKeyValueBasedCache(this IServiceCollection services) {
+    services.AddSingleton<IBufferDistributedCache, NatsKeyValueStoreBasedCache>(diContainer =>
+        CreateNatsKeyValueStoreBasedCache(
+          diContainer.GetRequiredService<INatsConnection>(),
+          diContainer.GetRequiredService<KeyValueBasedCacheSettings>(),
+          diContainer))
+      .AddSingleton<IDistributedCache>(diContainer => diContainer.GetRequiredService<IBufferDistributedCache>());
+    return services;
+  }
+
+  private static NatsObjectStoreBasedCache CreateNatsObjectStoreBasedCache(
+    INatsConnection natsClient,
+    ObjectStoreBasedCacheSettings settings,
+    IServiceProvider diContainer) {
+    var objectStoreContext = natsClient.CreateJetStreamContext()
+      .CreateObjectStoreContext();
+    var bucket = objectStoreContext.GetObjectStoreAsync(settings.BucketName).AsTask().GetAwaiter().GetResult();
+    var timeProvider = diContainer.GetRequiredService<TimeProvider>();
+    var expiryCalculator = new CacheEntryExpiryCalculator(settings.DefaultSlidingExpirationInterval, timeProvider);
+    var datastore = new ObjectStoreBasedDatastore(bucket, expiryCalculator);
+    var invalidation = new ObjectStoreBasedCacheInvalidation(
+      bucket,
+      settings.ExpiredEntriesPurgingInterval,
+      expiryCalculator,
+      timeProvider,
+      diContainer.GetRequiredService<ILogger<ObjectStoreBasedCacheInvalidation>>());
+    return new NatsObjectStoreBasedCache(
+      datastore,
+      invalidation,
+      diContainer.GetRequiredService<ILogger<NatsObjectStoreBasedCache>>());
+  }
+
+  private static NatsKeyValueStoreBasedCache CreateNatsKeyValueStoreBasedCache(
+    INatsConnection natsClient,
+    KeyValueBasedCacheSettings settings,
+    IServiceProvider diContainer) {
+    var keyValueStoreContext = natsClient.CreateJetStreamContext().CreateKeyValueStoreContext();
+    var valueStore = keyValueStoreContext.CreateStoreAsync(settings.ValueStore).AsTask().GetAwaiter().GetResult();
+    var metadataStore = keyValueStoreContext.CreateStoreAsync(settings.MetadataStore).AsTask().GetAwaiter().GetResult();
+    var timeProvider = diContainer.GetRequiredService<TimeProvider>();
+    var expiryCalculator = new CacheEntryExpiryCalculator(settings.DefaultSlidingExpirationInterval, timeProvider);
+    var expirySerializer = new CacheEntryExpiryJsonSerializer();
+    var datastore = new KeyValueBasedDatastore(
+      valueStore,
+      metadataStore,
+      expirySerializer,
+      expiryCalculator);
+    var invalidation = new KeyValueBasedCacheInvalidation(
+      valueStore,
+      metadataStore,
+      settings.ExpiredEntriesPurgingInterval,
+      expirySerializer,
+      expiryCalculator,
+      timeProvider,
+      diContainer.GetRequiredService<ILogger<KeyValueBasedCacheInvalidation>>());
+    return new NatsKeyValueStoreBasedCache(
+      datastore,
+      invalidation,
+      diContainer.GetRequiredService<ILogger<NatsKeyValueStoreBasedCache>>());
   }
 }
